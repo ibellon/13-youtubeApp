a83a11add950ad18c56ba8bb8e8ed925114ce2fa
ibellon <isidro.bellon@gmail.com>
2023-01-14 20:09:03 +0100

    Servicio y Modelo para extraer vídeos

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { YoutubeResponse } from '../models/youtube.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
+  
+  private youtubeUrl = "https://www.googleapis.com/youtube/v3";
+  private apiKey = "AIzaSyCuxTh0UmKq0M3Ei9TGfaL1ZNTwI07Bn7w";
+  private playlistId = "UUuaPTYj15JSkETGnEseaFFg";
+  private nextPageToken = "";
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getVideos() {
+
+    const url = this.youtubeUrl+"/playlistItems";
+    const params = new HttpParams()
+      .set("part", "snippet")
+      .set("maxResults", "10")
+      .set("playlistId", this.playlistId)
+      .set("key", this.apiKey);
+    
+    return this.http.get<YoutubeResponse>(url, { params })
+      .pipe(
+        map( resp => {
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }),
+        map(items => items.map( video => video.snippet))
+      );
+  }
 }
